32013c818e2fdbc9dff649af5edfe0a9451e4da2
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-10-28 18:23:30 +0100

    maj

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -7,6 +7,7 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+    
     const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
